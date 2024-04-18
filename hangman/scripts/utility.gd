f8c805970e5926a8df4627e4bd252a7ba7bdffce
Charles <CharlesCBostwick@gmail.com>
2024-04-18 10:27:37 +0100

    functioning barebones hangman game

---
+++ hangman/scripts/utility.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var default_dictionary_location: String = "user://dictionary.txt"
+
+# Function to open the dictionary file for reading. 
+func open_dictionary() -> FileAccess:
+	if FileAccess.file_exists(default_dictionary_location):
+		return FileAccess.open(default_dictionary_location, FileAccess.READ)
+	else:
+		return FileAccess.open(default_dictionary_location, FileAccess.WRITE_READ)
+
+# Function to save the word list to the dictionary file.
+func save_dictionary() -> void:
+	var file = FileAccess.open(default_dictionary_location, FileAccess.WRITE)
+	var string = ""
+	for word in WordList.words:
+		string += word
+		string += "\n"
+	file.store_string(string)
